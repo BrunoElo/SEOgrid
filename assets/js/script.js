dcb2f6816215b4415ce2a2d9041e6901be4fb5b0
Emeka Elo-Chukwuma <elochux@gmail.com>
2020-08-20 11:19:29 +0100

    added animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         //setting-name: setting-value
         dots: true,
         customPaging: function (slider, i) { //making your custom slick dots
-            return '<div class="custom-dots" id=' + i + "><span>" +0+ (i + 1) + "</span></div>";
+            return '<div class="custom-dots" id=' + i + "><span>" + 0 + (i + 1) + "</span></div>";
         },
         //infinite: false,
         //speed: 300,
@@ -13,7 +13,7 @@ $(document).ready(function () {
         autoplay: true,
         autoplaySpeed: 5000,
         arrows: false,
-        dotsClass:"vertical-dots"
+        dotsClass: "vertical-dots"
     });
 });
 
@@ -37,4 +37,41 @@ $(document).ready(function () {
         nextArrow: '<svg class="slick-next" width="70" height="70" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg"><g filter="url(#filter0_b)"><circle cx="35" cy="35" r="33.5" stroke="#00B071" stroke-width="3"/>  </g>  <path d="M49.668 35.3891L31.5846 45.8295L31.5846 24.9486L49.668 35.3891Z" fill="#00B071"/>  <defs>  <filter id="filter0_b" x="-5.6" y="-5.6" width="81.2" height="81.2" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">  <feFlood flood-opacity="0" result="BackgroundImageFix"/>  <feGaussianBlur in="BackgroundImage" stdDeviation="2.8"/>  <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur"/>  <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur" result="shape"/>  </filter>  </defs>  </svg>'
     });
 });
+
+// ScrollReveal animations start
+
+ScrollReveal().reveal('.navbar-brand', {
+    duration: 1000,
+    origin: 'left',
+    distance: '300px',
+    opacity: 0,
+    scale: 0.8
+});
+
+ScrollReveal().reveal('.intro__img, .sr-right', {
+    duration: 1000,
+    origin: 'right',
+    distance: '500px',
+    opacity: 0,
+    scale: 0.8
+});
+
+ScrollReveal().reveal('.intro, .sr-left', {
+    duration: 1000,
+    origin: 'left',
+    distance: '300px',
+    opacity: 0,
+    scale: 0.8
+});
+
+ScrollReveal().reveal('.campdash, .sr-bottom', {
+    duration: 1000,
+    //origin: 'left',
+    distance: '300px',
+    opacity: 0,
+    scale: 0.8
+});
+
+// ScrollReveal animations end
+
 
